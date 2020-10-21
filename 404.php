af0df9a59cb4f925f664d3bfd6cd707545ec7342
Pavel Itchin <itchin.pavel@yandex.ru>
2020-10-22 01:57:18 +0300

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<div class="container toys">
+    <h2 class='subtitle'>
+        <a href="<?php echo get_home_url(); ?>">
+            Такой страницы не существует. Вернуться назад? <br>
+            There's no such page. Return?
+        </a>
+    </h2>
+</div>
+
+<?php get_footer(); ?>
